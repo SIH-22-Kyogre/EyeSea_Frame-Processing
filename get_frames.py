21ad9c34974a4f44937ff2f401ce0e7c0866c94e
karthik-d <karthikdesingu2000@gmail.com>
2022-03-23 00:11:50 +0530

    fixed frame getter

--- get_frames.py
+++ get_frames.py
@@ -1,11 +1,25 @@
 import os 
 import shutil
 
+def is_exact_frame(file_, all_frames, pool_):
+    for frame, is_poss in zip(all_frames, pool_):
+        if is_poss:
+            start = file_.find(frame)
+            end = start + len(frame) - 1
+            if start<1 or (end+1)>=len(file_):
+                continue
+            # Check full match
+            if not(file_[start-1].isdigit() or file_[end+1].isdigit()):
+                return True
+    return False
+
 output_path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.path.pardir, 'data', 'output'))
 input_path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.path.pardir, 'data', 'input'))
 # redefine and uncomment below, for other paths
-input_path = "/home/karthikd/Workspace/MachineLearning/Projects/SIH'22/Frames-Preprocessing/Deduplication/data/fish_chengalpattutank_aug14"
+input_path = "/home/karthikd/Workspace/MachineLearning/Projects/SIH'22/Frames-Preprocessing/Deduplication/data/shrimp_dec20_para"
 
+"""
+# Chengalpattu Tank
 reqd_frame_nos = list(map(str, [
     16528,
     25317,
@@ -26,9 +40,28 @@ reqd_frame_nos = list(map(str, [
     30689,
     34069,
 ]))
+"""
 
-for frame_file in os.listdir(input_path):
-    if any([(reqd_frame in frame_file) for reqd_frame in reqd_frame_nos]):
+"""
+# Shrimp - Dec 20
+reqd_frame_nos = list(map(str, [
+    297,
+    313,
+    390,
+    401,
+    439,
+    521,
+    649,
+    688,
+    695
+]))
+"""
+
+
+frame_file_l = os.listdir(input_path)
+for frame_file in frame_file_l:
+    poss_pool = [(reqd_frame in frame_file) for reqd_frame in reqd_frame_nos]
+    if any(poss_pool) and is_exact_frame(frame_file, reqd_frame_nos, poss_pool):
         shutil.copy(
             os.path.join(input_path, frame_file),
             os.path.join(output_path, frame_file)
